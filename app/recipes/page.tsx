4a130ab2b226ea62b38d5c5ca1a6f3dded5d8813
Aaron Bradley <aaron.bradley@lm-k1w73533mj.home>
2024-04-08 17:57:11 +0100

    added db basics

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,7 +1,21 @@
-export default function Recipes() {
+import { db } from "../../db/db";
+import { users, SelectUser } from "../../db/schema";
+
+async function getAllUsers(): Promise<Array<SelectUser>> {
+  const res = await db.select().from(users);
+
+  return res;
+}
+
+export default async function Recipes() {
+  const userLists = await getAllUsers();
+
   return (
     <main>
-      <div>Recipes</div>
+      Recipes
+      {userLists.map(({ name, id }) => {
+        return <p key={id.toString()}>{name}</p>;
+      })}
     </main>
   );
 }
