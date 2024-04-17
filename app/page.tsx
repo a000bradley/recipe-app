13a549cf62202ef45caff950f2cabe56fd7a7173
Aaron Bradley <aaron.bradley@lm-k1w73533mj.home>
2024-04-17 22:11:28 +0100

    enable recupe list per day of the week

--- app/page.tsx
+++ app/page.tsx
@@ -6,37 +6,51 @@ import IngredientsList from "./components/IngredientsList/IngredientsList";
 
 export default function Home() {
   const [searchTerm, setsearchTerm] = React.useState("");
-  const [ingredients, setIngredients] = React.useState([]);
+  const [ingredients, setIngredients] = React.useState({
+    monday: [],
+    tuesday: [],
+    wednesday: [],
+    thursday: [],
+    friday: [],
+  });
+  // const [ingredients, setIngredients] = React.useState([]);
 
   function onChange(e: React.ChangeEvent<HTMLInputElement>) {
     setsearchTerm(e.target.value);
   }
 
-  async function handleButtonClick() {
+  async function handleButtonClick(dayOfWeek) {
     const response = await getIngredients(searchTerm);
 
     const ingredientArray = response.split(", ");
-    console.log(ingredientArray);
 
-    setIngredients(ingredientArray);
+    setIngredients((prevIngredients) => ({
+      ...prevIngredients,
+      [dayOfWeek]: ingredientArray,
+    }));
+
+    console.log(ingredients);
   }
 
   return (
     <main>
       <div className="container mx-auto">
-        <h1>Recipes App</h1>
+        <h1>Next Weeks ingredietns</h1>
 
-        <h2 className="text-5xl">Monday</h2>
-        <input
-          className="border-2"
-          placeholder="What are we having?"
-          name="monday"
-          onChange={(e) => onChange(e)}
-        ></input>
-        <button onClick={() => handleButtonClick()}>Search?</button>
-        <div>
-          <IngredientsList ingredients={ingredients} />
-        </div>
+        {Object.keys(ingredients).map((day) => (
+          <div key={day}>
+            <h2 className="text-5xl">{day}</h2>
+            <input
+              className="border-2"
+              placeholder="What are we having?"
+              name={day}
+              onChange={(e) => onChange(e)}
+            ></input>
+            <button onClick={() => handleButtonClick(day)}>Search?</button>
+            {/* ... your input */}
+            <IngredientsList ingredients={ingredients[day]} />
+          </div>
+        ))}
 
         <div className="fixed bottom-0 left-0 w-full bg-gray-100 p-4 flex justify-around items-center">
           Test
