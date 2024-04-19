95043e07528ceef57091436b273ef09d47d02fbc
Aaron Bradley <aaron.bradley@lm-k1w73533mj.home>
2024-04-19 19:26:49 +0100

    reduce cache revalidation

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Home from "./Home";
 import { getMealSchedule } from "./lib/mealSchedule";
 import initialMealPlan from "./utils/initialMealPlan";
 
+export const revalidate = 0;
+
 export default async function Page() {
   const mealSchedule = await getMealSchedule();
 
