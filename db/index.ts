6c6d7646bb1fbca8fae30a4af64646955cc4a925
Aaron Bradley <aaron.bradley@lm-k1w73533mj.home>
2024-04-19 10:41:00 +0100

    remove drizzel files

--- db/index.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import { resolve } from "node:path";
-import { migrate } from "drizzle-orm/libsql/migrator";
-import { db } from "./db";
-
-(async () => {
-  await migrate(db, { migrationsFolder: resolve(__dirname, "../migrations") });
-})();
--- migrations/0001_steep_maximus.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `recipes` (
-	`id` integer PRIMARY KEY NOT NULL,
-	`title` text NOT NULL,
-	`ingredients` text,
-	`method` text,
-	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
---> statement-breakpoint
-ALTER TABLE users ADD `extra` text;
